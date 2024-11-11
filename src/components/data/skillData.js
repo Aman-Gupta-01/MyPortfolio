c3070dc89e15d4aeb633a58db673e59784d70cf5
Aman Gupta <guptaman2007@gmail.com>
2024-11-11 11:16:40 +0530

    Updated skillData array with new order and npm after JSON

--- src/components/data/skillData.js
+++ src/components/data/skillData.js
@@ -1,45 +1,54 @@
 const skillData = [
-    {
-      id: 1,
-      name: "React",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"
-    },
-    {
-      id: 2,
-      name: "Redux",
-      image: "https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png"
-    },
-    {
-      id: 3,
-      name: "Node.js",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/512px-Node.js_logo.svg.png"
-    },
-    {
-      id: 4,
-      name: "JavaScript",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/512px-JavaScript-logo.png"
-    },
-    {
-      id: 5,
-      name: "HTML",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"
-    },
-    {
-      id: 6,
-      name: "CSS",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/512px-CSS3_logo_and_wordmark.svg.png"
-    },
-    {
-      id: 7,
-      name: "Vite",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/512px-Vitejs-logo.svg.png"
-    },
-    {
-      id: 8,
-      name: "GitHub",
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/512px-Octicons-mark-github.svg.png"
-    }
-  ];
-  
-  export default skillData;
-  
+  {
+    id: 1,
+    name: "React",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"
+  },
+  {
+    id: 2,
+    name: "Vite",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/512px-Vitejs-logo.svg.png"
+  },
+  {
+    id: 3,
+    name: "Redux Toolkit",
+    image: "https://upload.wikimedia.org/wikipedia/commons/4/49/Redux.png" // Placeholder - official Redux icon
+  },
+  {
+    id: 4,
+    name: "Tailwind CSS",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
+  },
+  {
+    id: 5,
+    name: "JSON",
+    image: "https://upload.wikimedia.org/wikipedia/commons/c/c9/JSON_vector_logo.svg"
+  },
+  {
+    id: 6,
+    name: "npm",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/540px-Npm-logo.svg.png"
+  },
+  {
+    id: 7,
+    name: "HTML",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"
+  },
+  {
+    id: 8,
+    name: "CSS",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/512px-CSS3_logo_and_wordmark.svg.png"
+  },
+  {
+    id: 9,
+    name: "JavaScript",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/512px-JavaScript-logo.png"
+  },
+  {
+    id: 10,
+    name: "GitHub",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/512px-Octicons-mark-github.svg.png"
+  },
+];
+
+export default skillData;
